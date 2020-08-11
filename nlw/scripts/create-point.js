1989c452df75345e582545ef10c07322cf0e86e6
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-08-11 18:35:48 -0300

    Visual do Itens de coleta

--- nlw/scripts/create-point.js
+++ nlw/scripts/create-point.js
@@ -18,11 +18,14 @@ function getCities(event) {
     const indexOfSelectedState = event.target.selectedIndex;
     stateInput.value = event.target.options[indexOfSelectedState].text;
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    citySelect.innerHTML = "<option value> Selcione sua cidade</option>"
+    citySelect.disabled = true;
     fetch(url)
+
     .then(res => res.json())
     .then(cities => {
         for(const city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
         citySelect.disabled = false;
     });
@@ -30,3 +33,13 @@ function getCities(event) {
 
 document.querySelector("select[name=uf]")
         .addEventListener("change", getCities); 
+
+const itensToCollect = document.querySelectorAll(".itens-grid li");
+
+for(let item of itensToCollect){
+    item.addEventListener("click", handleSelectedItem);
+}
+
+function handleSelectedItem(event) {
+    console.log(event.target);
+}
